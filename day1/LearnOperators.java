978f26bbddf5ec82b15fcec552e3d1b99321e487
Kalai-18 <153903066+Kalai-18@users.noreply.github.com>
2023-12-15 15:24:56 +0100

    Add files via upload
    Week 1 assignments

---
+++ day1/LearnOperators.java
@@ -0,0 +1,30 @@
+package week1.day1;
+
+public class LearnOperators {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 9;
+		float c = a/b ;
+		System.out.println(a+b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		System.out.println(c);
+		
+		System.out.println(a==b);	
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a<b && a>b);
+		System.out.println(a<b || a>b);
+		System.out.println(a !=b);
+		
+		System.out.println(a++);
+		System.out.println(a);
+		System.out.println(++a);
+		System.out.println(a--);
+		System.out.println(a);
+	
+	
+	}
+
+} 
